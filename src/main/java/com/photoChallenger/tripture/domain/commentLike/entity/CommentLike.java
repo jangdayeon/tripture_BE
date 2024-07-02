90804dd59577e629ce831b989d84090948cfd4ef
jangdayeon <dayeonjang.dev@gmail.com>
2024-07-02 20:56:56 +0900

    KAN-8 Feat : add CommentLike entity

---
+++ src/main/java/com/photoChallenger/tripture/domain/commentLike/entity/CommentLike.java
@@ -0,0 +1,44 @@
+package com.photoChallenger.tripture.domain.commentLike.entity;
+
+import com.photoChallenger.tripture.domain.comment.entity.Comment;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CommentLike {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentLikeId;
+
+    @Column(nullable = false)
+    private Long profileId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    private CommentLike(Long commentLikeId, Long profileId) {
+        this.commentLikeId = commentLikeId;
+        this.profileId = profileId;
+    }
+
+    @Builder
+    public static CommentLike create(Comment comment, Long commentLikeId, Long profileId){
+        CommentLike commentLike = new CommentLike(commentLikeId,profileId);
+        commentLike.addComment(comment);
+        return commentLike;
+    }
+
+    private void addComment(Comment comment){
+        this.comment = comment;
+        comment.getCommentLike().add(this);
+    }
+
+    public void remove(Comment comment){
+        comment.getCommentLike().remove(this);
+    }
+}
