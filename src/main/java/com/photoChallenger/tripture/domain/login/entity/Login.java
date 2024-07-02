aadc3b42f328a9bc87ea68e544bef5136592a0cf
jangdayeon <dayeonjang.dev@gmail.com>
2024-07-02 20:57:14 +0900

    KAN-8 Feat : add Login entity

---
+++ src/main/java/com/photoChallenger/tripture/domain/login/entity/Login.java
@@ -0,0 +1,42 @@
+package com.photoChallenger.tripture.domain.login.entity;
+
+import com.photoChallenger.tripture.domain.profile.entity.Profile;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Login {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loginId;
+
+    @Column(nullable = false, length = 320)
+    private String loginEmail;
+
+    @Column(nullable = false, length = 30)
+    private String loginPw;
+
+    @OneToOne(mappedBy = "login", fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL, orphanRemoval = true)
+    private Profile profile;
+
+    private Login(String loginEmail, String loginPw){
+        this.loginEmail = loginEmail;
+        this.loginPw = loginPw;
+
+    }
+
+    @Builder
+    public static Login create(String loginEmail, String loginPw){
+        Login login = new Login(loginEmail,loginPw);
+        return login;
+    }
+
+    protected void setProfile(Profile profile){
+        this.profile = profile;
+    }
+}
