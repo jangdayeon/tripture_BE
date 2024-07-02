22586b7408d94ed397ce18957aed21fa23813a1d
choijimin <jiminchoi1221@gmail.com>
2024-07-03 01:06:59 +0900

    KAN-9 Feat: Bookmark Builder, , 연관관계 편의 메소드 ì 작성

--- src/main/java/com/photoChallenger/tripture/domain/bookmark/entity/Bookmark.java
+++ src/main/java/com/photoChallenger/tripture/domain/bookmark/entity/Bookmark.java
@@ -3,8 +3,11 @@ package com.photoChallenger.tripture.domain.bookmark.entity;
 import com.photoChallenger.tripture.domain.profile.entity.Profile;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.awt.print.Book;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -20,4 +23,21 @@ public class Bookmark {
     @ManyToOne
     @JoinColumn(name = "profile_id")
     private Profile profile;
+
+    private Bookmark(Long bookmarkId, String contentId) {
+        this.BookmarkId = bookmarkId;
+        this.ContentId = contentId;
+    }
+
+    @Builder
+    public static Bookmark create(Profile profile, Long bookmarkId, String contentId) {
+        Bookmark bookmark = new Bookmark(bookmarkId, contentId);
+        bookmark.addProfile(profile);
+        return bookmark;
+    }
+
+    private void addProfile(Profile profile) {
+        this.profile = profile;
+        profile.getBookmarks().add(this);
+    }
 }
