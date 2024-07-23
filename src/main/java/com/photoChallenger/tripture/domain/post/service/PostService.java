6f59d8943daf5186b9c559528acbc378d9bd618d
choijimin <jiminchoi1221@gmail.com>
2024-07-23 22:48:18 +0900

    KAN-31 Rename: 도메인 별 Service -> Interface 로 변경

--- src/main/java/com/photoChallenger/tripture/domain/post/service/PostService.java
+++ src/main/java/com/photoChallenger/tripture/domain/post/service/PostService.java
@@ -1,11 +1,4 @@
 package com.photoChallenger.tripture.domain.post.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class PostService {
+public interface PostService {
 }
