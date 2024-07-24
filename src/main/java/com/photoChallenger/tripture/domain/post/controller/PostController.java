2a0ecfb330f755f25d83ab8da192723b4e14ca12
jangdayeon <dayeonjang.dev@gmail.com>
2024-07-25 04:17:25 +0900

    KAN-57 Feat: 내가 작성한 게시글 리스트 조회 API 구현_controller

--- src/main/java/com/photoChallenger/tripture/domain/post/controller/PostController.java
+++ src/main/java/com/photoChallenger/tripture/domain/post/controller/PostController.java
@@ -1,11 +1,30 @@
 package com.photoChallenger.tripture.domain.post.controller;
 
+import com.photoChallenger.tripture.domain.login.dto.LoginIdResponse;
+import com.photoChallenger.tripture.domain.login.entity.SessionConst;
+import com.photoChallenger.tripture.domain.post.dto.MyPostResponse;
+import com.photoChallenger.tripture.domain.post.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/post")
 public class PostController {
+    private final PostService postService;
+    @GetMapping("/myPostList")
+    public ResponseEntity<List<MyPostResponse>> findMyPostList(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        LoginIdResponse loginIdResponse = (LoginIdResponse) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        return ResponseEntity.ok().body(postService.findMyPosts(loginIdResponse.getLoginId()));
+    }
 }
