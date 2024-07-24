50f52c66ed5a44e1c1afbe6d5da134e1ae8d7010
jangdayeon <dayeonjang.dev@gmail.com>
2024-07-25 03:17:32 +0900

    KAN-53 Feat: 내가 저장한 관광지&챌린지 리스트 최신순 조회_service

--- src/main/java/com/photoChallenger/tripture/domain/bookmark/service/BookmarkService.java
+++ src/main/java/com/photoChallenger/tripture/domain/bookmark/service/BookmarkService.java
@@ -1,4 +1,15 @@
 package com.photoChallenger.tripture.domain.bookmark.service;
 
+import com.photoChallenger.tripture.domain.bookmark.dto.MyContentResponse;
+import com.photoChallenger.tripture.domain.bookmark.dto.MyPhotoChallengeResponse;
+
+import java.util.List;
+
 public interface BookmarkService {
+
+    //내가 저장한 관광지 리스트
+    public List<MyContentResponse> getContentList(Long loginId);
+
+    //내가 저장한 챌린지 리스트
+    public List<MyPhotoChallengeResponse> getPhotoChallengeList(Long loginId);
 }
