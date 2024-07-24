a26a0f572cddace9cac792fc7cb95a3c72f745d6
jangdayeon <dayeonjang.dev@gmail.com>
2024-07-24 16:40:36 +0900

    KAN-40 Feat: 회원 수정, 회원 수정 폼, 회원 기본 정보 조회, 챌린저 레벨 조회 API 구현

--- src/main/java/com/photoChallenger/tripture/domain/profile/controller/ProfileController.java
+++ src/main/java/com/photoChallenger/tripture/domain/profile/controller/ProfileController.java
@@ -1,11 +1,78 @@
 package com.photoChallenger.tripture.domain.profile.controller;
 
+import com.photoChallenger.tripture.domain.login.dto.LoginIdResponse;
+import com.photoChallenger.tripture.domain.login.entity.SessionConst;
+import com.photoChallenger.tripture.domain.profile.dto.MemberDto;
+import com.photoChallenger.tripture.domain.profile.dto.MemberEditForm;
+import com.photoChallenger.tripture.domain.profile.dto.MemberEditRequest;
+import com.photoChallenger.tripture.domain.profile.service.ProfileService;
+import com.photoChallenger.tripture.global.S3.S3Service;
+import com.photoChallenger.tripture.global.exception.global.S3IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/profile")
 public class ProfileController {
+    private final ProfileService profileService;
+    private final S3Service s3Service;
+    //회원 기본 정보 조회
+    @GetMapping("/default")
+    public ResponseEntity<MemberDto> defaultMember(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        LoginIdResponse loginIdResponse = (LoginIdResponse) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        return ResponseEntity.ok().body(profileService.getMember(loginIdResponse.getLoginId()));
+    }
+
+    //회원 수정 폼
+    @GetMapping("/edit")
+    public ResponseEntity<MemberEditForm> editMemeberForm(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        LoginIdResponse loginIdResponse = (LoginIdResponse) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        return ResponseEntity.ok().body(profileService.memberEditForm(loginIdResponse.getLoginId()));
+    }
+
+    //회원 수정
+    @PutMapping("/edit")
+    public ResponseEntity<String> editMember(HttpServletRequest request, MemberEditRequest memberEditRequest){
+        HttpSession session = request.getSession(false);
+        LoginIdResponse loginIdResponse = (LoginIdResponse) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        String imgName = "default";
+        // 사진 등록
+        if(!memberEditRequest.getFile().isEmpty()) {
+            try {
+                String checkProfileImgName = profileService.checkProfileImgName(loginIdResponse.getLoginId());
+                if (checkProfileImgName != null){
+                    s3Service.delete(checkProfileImgName);
+                }
+                imgName = s3Service.upload(memberEditRequest.getFile(), "profile");
+            } catch (IOException e){
+                throw new S3IOException();
+            }
+        }
+
+        profileService.memberEdit(imgName, memberEditRequest.getProfileNickname(), memberEditRequest.getLoginPw(), loginIdResponse.getLoginId());
+        return new ResponseEntity("redirection request", HttpStatus.SEE_OTHER);
+    }
+
+    //챌린저 레벨 조회
+    @GetMapping("/checkLevel")
+    public ResponseEntity<String> checkChallengeLevel(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        LoginIdResponse loginIdResponse = (LoginIdResponse) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        return ResponseEntity.ok().body(profileService.checkLevel(loginIdResponse.getLoginId()));
+    }
 }
--- src/main/java/com/photoChallenger/tripture/domain/profile/service/ProfileServiceImpl.java
+++ src/main/java/com/photoChallenger/tripture/domain/profile/service/ProfileServiceImpl.java
@@ -1,11 +1,56 @@
 package com.photoChallenger.tripture.domain.profile.service;
 
+import com.photoChallenger.tripture.domain.login.entity.Login;
+import com.photoChallenger.tripture.domain.login.entity.LoginType;
+import com.photoChallenger.tripture.domain.login.repository.LoginRepository;
+import com.photoChallenger.tripture.domain.profile.dto.MemberDto;
+import com.photoChallenger.tripture.domain.profile.dto.MemberEditForm;
+import com.photoChallenger.tripture.domain.profile.dto.MemberEditRequest;
+import com.photoChallenger.tripture.domain.profile.entity.Profile;
+import com.photoChallenger.tripture.domain.profile.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProfileServiceImpl implements ProfileService{
-}
+    private final LoginRepository loginRepository;
+    @Override
+    public MemberDto getMember(Long LoginId){
+        Login login = loginRepository.findById(LoginId).get();
+        return MemberDto.from(login);
+    }
+
+    @Override
+    public MemberEditForm memberEditForm(Long loginId) {
+        Login login = loginRepository.findById(loginId).get();
+        return MemberEditForm.from(login);
+    }
+
+    @Override
+    @Transactional
+    public void memberEdit(String profileImgName, String profileNickname, String loginPw, long loginId) {
+        Login login = loginRepository.findById(loginId).get();
+        Profile profile = login.getProfile();
+        profile.update(profileImgName,profileNickname);
+        if (login.getLoginType().equals(LoginType.SELF)){
+            login.update(loginPw);
+        }
+    }
+
+    @Override
+    public String checkLevel(Long loginId) {
+        Login login = loginRepository.findById(loginId).get();
+        return login.getProfile().getProfileLevel().toString();
+    }
+
+    @Override
+    public String checkProfileImgName(Long loginId) {
+        Login login = loginRepository.findById(loginId).get();
+        return login.getProfile().getProfileImgName() != "default"? login.getProfile().getProfileImgName():null;
+        }
+    }
