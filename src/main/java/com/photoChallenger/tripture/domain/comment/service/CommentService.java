edca653b8c3108d97fd8fa1ee97a3df704d77d1b
jangdayeon <dayeonjang.dev@gmail.com>
2024-07-25 03:57:21 +0900

    KAN-55 Feat: 작성한 댓글 리스트 API 구현_service

--- src/main/java/com/photoChallenger/tripture/domain/comment/service/CommentService.java
+++ src/main/java/com/photoChallenger/tripture/domain/comment/service/CommentService.java
@@ -1,4 +1,9 @@
 package com.photoChallenger.tripture.domain.comment.service;
 
+import com.photoChallenger.tripture.domain.comment.dto.MyCommentResponse;
+
+import java.util.List;
+
 public interface CommentService {
+    public List<MyCommentResponse> findMyComments(Long loginId);
 }
