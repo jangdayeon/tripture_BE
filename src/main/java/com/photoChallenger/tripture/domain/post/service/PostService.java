9f2a1ef76f9453ffbe6e10bff6c97286306919a4
jangdayeon <dayeonjang.dev@gmail.com>
2024-07-25 04:17:44 +0900

    KAN-57 Feat: 내가 작성한 게시글 리스트 조회 API 구현_service

--- src/main/java/com/photoChallenger/tripture/domain/post/service/PostService.java
+++ src/main/java/com/photoChallenger/tripture/domain/post/service/PostService.java
@@ -1,4 +1,9 @@
 package com.photoChallenger.tripture.domain.post.service;
 
+import com.photoChallenger.tripture.domain.post.dto.MyPostResponse;
+
+import java.util.List;
+
 public interface PostService {
+    public List<MyPostResponse> findMyPosts(Long loginId);
 }
