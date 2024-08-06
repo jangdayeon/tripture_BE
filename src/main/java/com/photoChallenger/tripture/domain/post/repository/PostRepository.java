5eca006ebdcc7c324d30031b7ac065d730693d72
jangdayeon <dayeonjang.dev@gmail.com>
2024-08-07 00:45:09 +0900

    KAN-77 Feat: 내가 작성한 게시글 목록 API 페이징 구현

--- src/main/java/com/photoChallenger/tripture/domain/post/repository/PostRepository.java
+++ src/main/java/com/photoChallenger/tripture/domain/post/repository/PostRepository.java
@@ -1,10 +1,11 @@
 package com.photoChallenger.tripture.domain.post.repository;
 
 import com.photoChallenger.tripture.domain.post.entity.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    public List<Post> findAllByProfile_ProfileIdOrderByPostDateAsc(Long profileId);
+    public List<Post> findAllByProfile_ProfileId(Long profileId, Pageable pageable);
 }
