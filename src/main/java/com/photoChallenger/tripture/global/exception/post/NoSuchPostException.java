f7b752525062076d9ff992dde88bdbb542ee91da
choijimin <jiminchoi1221@gmail.com>
2024-08-07 20:20:30 +0900

    KAN-80 Feat: 댓글 작성 API 구현

---
+++ src/main/java/com/photoChallenger/tripture/global/exception/post/NoSuchPostException.java
@@ -0,0 +1,12 @@
+package com.photoChallenger.tripture.global.exception.post;
+
+import com.photoChallenger.tripture.global.exception.TriptureException;
+import org.springframework.http.HttpStatus;
+
+public class NoSuchPostException extends TriptureException {
+    private static final String MESSAGE = "존재하지 않는 게시물입니다.";
+
+    public NoSuchPostException() {
+        super(MESSAGE, HttpStatus.NOT_FOUND);
+    }
+}
