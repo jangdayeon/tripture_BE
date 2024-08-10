13fd135dc5e3604a7883d5b4668278395cd824ef
jangdayeon <dayeonjang.dev@gmail.com>
2024-08-10 20:46:35 +0900

    KAN-96 Fix: 변수명 수정

--- src/main/java/com/photoChallenger/tripture/domain/challenge/entity/ChallengeRegion.java
+++ src/main/java/com/photoChallenger/tripture/domain/challenge/entity/ChallengeRegion.java
@@ -1,5 +1,14 @@
 package com.photoChallenger.tripture.domain.challenge.entity;
 
+/**
+ * 인천,경기 - inc
+ * 서울 - seo
+ * 전라남도, 전북특별자치도,광주 - jeon
+ * 강원특별자치도 - gang
+ * 대전,세종특별자치시, 충청남도, 충청북도 - chung
+ * 경상북도, 경상남도, 부산, 대구, 울산 - gyeong
+ * 제주도 - je
+ */
 public enum ChallengeRegion {
-    서울,부산,인천,대구,대전,광주,울산,세종,경기,강원,충북,충남,전북,전남,경북,경남,제주
+    inc,seo,jeon,gang,chung,gyeong,je
 }
