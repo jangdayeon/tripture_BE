13346e337ddea0c00b8b168d9f9a515ecc8314dd
choijimin <jiminchoi1221@gmail.com>
2024-08-13 01:09:09 +0900

    KAN-110 Feat: 포토챌린지 진행 여부 확인 API 구현

--- src/main/java/com/photoChallenger/tripture/domain/challenge/repository/ChallengeRepository.java
+++ src/main/java/com/photoChallenger/tripture/domain/challenge/repository/ChallengeRepository.java
@@ -4,4 +4,5 @@ import com.photoChallenger.tripture.domain.challenge.entity.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+    Boolean existsByContentId(String contentId);
 }
--- src/main/java/com/photoChallenger/tripture/domain/challenge/service/ChallengeServiceImpl.java
+++ src/main/java/com/photoChallenger/tripture/domain/challenge/service/ChallengeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.photoChallenger.tripture.domain.challenge.service;
 
+import com.photoChallenger.tripture.domain.challenge.repository.ChallengeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,5 +8,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class ChallengeServiceImpl {
+public class ChallengeServiceImpl implements ChallengeService {
+    private final ChallengeRepository challengeRepository;
+
+    @Override
+    public boolean isPhotoChallengeExist(String contentId) {
+        return challengeRepository.existsByContentId(contentId);
+    }
 }
