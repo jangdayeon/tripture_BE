f025ef45fbb6a7a148c0bd42d23b8cfba8bb0b4a
choijimin <jiminchoi1221@gmail.com>
2024-08-13 14:54:28 +0900

    KAN-117 Feat: 포토챌린지 상세 정보 조회 API 구현

--- src/main/java/com/photoChallenger/tripture/domain/challenge/service/ChallengeService.java
+++ src/main/java/com/photoChallenger/tripture/domain/challenge/service/ChallengeService.java
@@ -1,6 +1,10 @@
 package com.photoChallenger.tripture.domain.challenge.service;
 
+import com.photoChallenger.tripture.domain.challenge.dto.GetPhotoChallengeResponse;
+
 public interface ChallengeService {
 
     boolean isPhotoChallengeExist(String contentId);
+
+    GetPhotoChallengeResponse getPhotoChallenge(String contentId, Long loginId);
 }
--- src/main/java/com/photoChallenger/tripture/domain/challenge/service/ChallengeServiceImpl.java
+++ src/main/java/com/photoChallenger/tripture/domain/challenge/service/ChallengeServiceImpl.java
@@ -1,6 +1,13 @@
 package com.photoChallenger.tripture.domain.challenge.service;
 
+import com.photoChallenger.tripture.domain.challenge.dto.GetPhotoChallengeResponse;
+import com.photoChallenger.tripture.domain.challenge.entity.Challenge;
 import com.photoChallenger.tripture.domain.challenge.repository.ChallengeRepository;
+import com.photoChallenger.tripture.domain.login.entity.Login;
+import com.photoChallenger.tripture.domain.login.repository.LoginRepository;
+import com.photoChallenger.tripture.domain.post.repository.PostRepository;
+import com.photoChallenger.tripture.global.exception.challenge.NoSuchChallengeException;
+import com.photoChallenger.tripture.global.exception.login.NoSuchLoginException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,9 +17,28 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ChallengeServiceImpl implements ChallengeService {
     private final ChallengeRepository challengeRepository;
+    private final PostRepository postRepository;
+    private final LoginRepository loginRepository;
 
     @Override
     public boolean isPhotoChallengeExist(String contentId) {
         return challengeRepository.existsByContentId(contentId);
+    }
+
+    @Override
+    public GetPhotoChallengeResponse getPhotoChallenge(String contentId, Long loginId) {
+        Login login = loginRepository.findById(loginId).orElseThrow(NoSuchLoginException::new);
+        Challenge challenge = challengeRepository.findByContentId(contentId).orElseThrow(NoSuchChallengeException::new);
+
+        Boolean isChallengeParticipate = postRepository.existsByProfile_ProfileIdAndChallenge_ChallengeId(login.getProfile().getProfileId()
+                , challenge.getChallengeId());
+
+        return GetPhotoChallengeResponse.builder()
+                .challengeId(challenge.getChallengeId())
+                .challengeName(challenge.getChallengeName())
+                .challengeImgName(challenge.getChallengeImgName())
+                .challengeContent(challenge.getChallengeContent())
+                .challengePoint(challenge.getChallengePoint())
+                .isChallengeParticipate(isChallengeParticipate).build();
     }
 }
