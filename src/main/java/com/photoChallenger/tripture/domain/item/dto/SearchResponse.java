9ffa07abaee350fd89ed13a211765d4a3a43865b
jangdayeon <dayeonjang.dev@gmail.com>
2024-08-17 15:55:47 +0900

    KAN-95 Fix: S3 DTO 연결 수정

--- src/main/java/com/photoChallenger/tripture/domain/item/dto/SearchResponse.java
+++ src/main/java/com/photoChallenger/tripture/domain/item/dto/SearchResponse.java
@@ -1,16 +1,24 @@
 package com.photoChallenger.tripture.domain.item.dto;
 
+import com.photoChallenger.tripture.global.S3.S3Url;
 import lombok.*;
 
 @Data
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class SearchResponse {
     Long itemId;
     String itemImgName;
     String itemName;
     String itemPosition;
     Integer itemPrice;
+
+    public SearchResponse(Long itemId, String itemImgName, String itemName, String itemPosition, Integer itemPrice) {
+        this.itemId = itemId;
+        this.itemImgName = S3Url.S3_URL + itemImgName;
+        this.itemName =  itemName;
+        this.itemPosition = itemPosition;
+        this.itemPrice = itemPrice;
+    }
 }
--- src/main/java/com/photoChallenger/tripture/domain/post/dto/SearchResponse.java
+++ src/main/java/com/photoChallenger/tripture/domain/post/dto/SearchResponse.java
@@ -1,13 +1,18 @@
 package com.photoChallenger.tripture.domain.post.dto;
 
+import com.photoChallenger.tripture.global.S3.S3Url;
 import lombok.*;
 
 @Data
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class SearchResponse {
     Long postId;
     String postImgName;
+
+    public SearchResponse(Long postId, String postImgName) {
+        this.postId = postId;
+        this.postImgName = S3Url.S3_URL + postImgName;
+    }
 }
