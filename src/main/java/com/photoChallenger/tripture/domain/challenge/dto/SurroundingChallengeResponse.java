55fe6ebd4b8533f40c3527fb0561984ce707d8e0
jangdayeon <dayeonjang.dev@gmail.com>
2024-08-18 00:35:19 +0900

    KAN-132 Fix: 포토챌린지-주변 포토챌린지 리스트 API 구현

--- src/main/java/com/photoChallenger/tripture/domain/challenge/dto/SurroundingChallengeResponse.java
+++ src/main/java/com/photoChallenger/tripture/domain/challenge/dto/SurroundingChallengeResponse.java
@@ -4,6 +4,8 @@ import com.photoChallenger.tripture.domain.challenge.entity.ChallengeRegion;
 import com.photoChallenger.tripture.global.S3.S3Url;
 import lombok.*;
 
+import java.util.Arrays;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,17 +14,20 @@ import lombok.*;
 public class SurroundingChallengeResponse {
     private Long challengeId;
     private String challengeImgName;
-    private String challengeName;
-    private ChallengeRegion challengeRegion;
+    private String challengePoint;
+    private String challengeRegion;
     private Long distance_meter;
     private Long participants;
 
     public static SurroundingChallengeResponse of(ChallengeAppendDistanceDto challengeAppendDistanceDto, Long participants){
+        String[] challengeName = challengeAppendDistanceDto.getChallenge().getChallengeName().split(" ");
+        String region = challengeName[0]; //챌린지명에서 챌린지 장소 파싱
+        String point = String.join(" ",Arrays.copyOfRange(challengeName,1,challengeName.length));
         return SurroundingChallengeResponse.builder()
                 .challengeId(challengeAppendDistanceDto.getChallenge().getChallengeId())
                 .challengeImgName(S3Url.S3_URL+challengeAppendDistanceDto.getChallenge().getChallengeImgName())
-                .challengeName(challengeAppendDistanceDto.getChallenge().getChallengeName())
-                .challengeRegion(challengeAppendDistanceDto.getChallenge().getChallengeRegion())
+                .challengePoint(point)
+                .challengeRegion(region)
                 .distance_meter(Math.round(challengeAppendDistanceDto.getDistance()))
                 .participants(participants)
                 .build();
